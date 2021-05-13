9b0f8e7e287c43a91cc7debe85914a4d064065f0
Matas Lauzadis <matas.lauzadis@gmail.com>
2021-05-13 12:06:45 -0500

    Upload Palo Alto data

--- project/datastructures.py
+++ project/datastructures.py
@@ -3,6 +3,10 @@ from typing import List
 
 @dataclass
 class Site:
+    '''
+    This can represent things like buildings, restaurants, and parks.
+    (e.g. Sears Tower, Chicago House of Blues, Millenium Park)
+    '''
     name: str
     link: str
     total_posts: int
@@ -11,6 +15,11 @@ class Site:
 
 @dataclass
 class Location:
+    '''
+    This can represent things like cities, neighborhoods, and bouroughs.
+    (e.g. Chicago, Manhattan, Las Vegas)
+    '''
+
     name: str
     link: str
     sites: List[Site]
